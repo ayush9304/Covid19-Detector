b0d1a55140132b9c460c51dfe9a4c75635a7996b
Ayush Kumar <ayush.kmr930@gmail.com>
2021-10-31 06:25:03 +0530

    Done iOS Image API

--- covid19detector/main/urls.py
+++ covid19detector/main/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('api/image', views.api_image, name='api_image'),
+    path('api/ios/image', views.ios_api_image, name='ios_api_image'),
 ]
--- covid19detector/main/views.py
+++ covid19detector/main/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
 from .models import Patient
+from django.conf import settings
 
 
 import tensorflow as tf
@@ -9,16 +10,21 @@ import numpy as np
 from PIL import Image
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
+from django.core.files.base import ContentFile
+import random
+import string
+import base64
+import os
 
 
 def get_covid19_classifier():
 	model = load_model('models\\covid19_pneumonia_classifier.h5')
-	model.summary()
+	# model.summary()
 	return model
 
 def get_xray_validator():
 	model = load_model('models\\xray_validator.h5')
-	model.summary()
+	# model.summary()
 	return model
 
 classifier = get_covid19_classifier()
@@ -36,8 +42,11 @@ validation_class = {
 }
 
 
-def validate(file) -> bool:
-    img = Image.open(file.file)
+def validate(file, isurl=False) -> bool:
+    if not isurl:
+        img = Image.open(file.file)
+    else:
+        img = Image.open(file)
     img = img.convert('RGB')
     img = img.resize((150, 150), Image.NEAREST)
     img = image.img_to_array(img)
@@ -45,8 +54,11 @@ def validate(file) -> bool:
     prediction = validator.predict(img)[0][0]
     return prediction>=0.5
 
-def classify(file):
-    img = Image.open(file.file)
+def classify(file, isurl=False):
+    if not isurl:
+        img = Image.open(file.file)
+    else:
+        img = Image.open(file)
     img = img.convert('RGB')
     img = img.resize((180, 180), Image.NEAREST)
     img = image.img_to_array(img)
@@ -100,4 +112,60 @@ def api_image(request):
             'prediction': data.prediction,
             'image_url': "127.0.0.1:8000/media/" + str(data.xray)
         })
+    else:
+        return JsonResponse({
+            'success': False,
+            'description': "No image found"
+        })
 
+@csrf_exempt
+def ios_api_image(request):
+    if request.method != "POST":
+        return JsonResponse({
+            'success': False,
+            'description': "Request method must be POST"
+        })
+
+    if request.POST.get('image'):
+        imgstr = request.POST.get('image')
+        imgstr = imgstr.replace(" ", "+")
+
+        filename = ''.join(random.choices(string.ascii_letters + string.digits, k=16)) + ".jpg"
+        imgdata = ContentFile(base64.b64decode(imgstr))
+        
+        data = Patient.objects.create()
+        data.xray.save(filename, imgdata, save=True)  # xray is Patient model's ImageField
+
+        img_url = data.xray.file
+        
+        if validate(os.path.join(str(img_url)), isurl=True):
+            prediction, covid_percentage, normal_percentage, pneumonia_percentage = classify(os.path.join(str(img_url)), isurl=True)
+            data.prediction = prediction
+            data.covid_percentage = covid_percentage
+            data.normal_percentage = normal_percentage
+            data.pneumonia_percentage = pneumonia_percentage
+            data.save()
+
+            return JsonResponse({
+                'success': True,
+                'method': request.method,
+                'name': data.name,
+                'description': "Successfully uploaded",
+                'covid_percentage': data.covid_percentage,
+                'normal_percentage': data.normal_percentage,
+                'pneumonia_percentage': data.pneumonia_percentage,
+                'prediction': data.prediction,
+                'image_url': "127.0.0.1:8000/media/" + str(data.xray),
+                # 'image_url': "https://api-ayush.herokuapp.com/media/predictions/" + str(filename)
+            })
+        else:
+            data.delete()
+            return JsonResponse({
+                'success': False,
+                'description': "Not an X-Ray image"
+            })
+    else:
+        return JsonResponse({
+            'success': False,
+            'description': "No image found"
+        })
